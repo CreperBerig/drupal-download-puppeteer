f8cbe47b9336a30798f5d8a33c958c6e7391003b
CreperBerig <rcreper135@gmail.com>
2025-07-18 15:40:13 +0300

    commit

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import puppeteer, { Page } from 'puppeteer';
-import { checkbox } from '@inquirer/prompts';
+import { checkbox, select } from '@inquirer/prompts';
 
 async function press(page: Page, key: string) {
     await page.waitForSelector(key);
@@ -12,42 +12,50 @@ async function press(page: Page, key: string) {
     });
     const page = await browser.newPage();
     await page.goto('http://localhost:8080/', {waitUntil: 'domcontentloaded'});
-    if (await page.$('a[href="/core/install.php?continue=1"]')) {
-        await press(page, 'a[href="/core/install.php?continue=1"]');
-    }
-
-    await page.waitForSelector(`label[class="form-item__label option"]`)
-    const labels = await page.evaluate(() => 
-        Array.from(document.querySelectorAll('.form-item__label'))
-            .map(el => el.textContent?.trim())
-    );
-    const checkboxLabels = await page.evaluate(() =>
-        Array.from(document.querySelectorAll('.form-checkbox'))
-            .map(el => el.dataset.drupalSelector)
-    );
-    console.log(labels);
-    console.log(checkboxLabels);
-
-    const selections = await checkbox({
-        message: 'You can select pre-configured types of content now (you can add them later)',
-        choices: labels.map((label, index) => 
-            ({value: index, name: label})
-        ),
-        pageSize: labels.length,
-    });
-
-    if( selections.length > 0 ) {
-        for (const index of selections) {
-            console.log(`Selecting checkbox for:\t${labels[index]}\t${checkboxLabels[index]}`);
-            const selector = `input[data-drupal-selector="${checkboxLabels[index]}"]`;
-            await page.evaluate(selector => {
-                const el = document.querySelector(selector) as HTMLInputElement | null;
-                if (el) el.click();
-            }, `input[data-drupal-selector="${checkboxLabels[index]}"]`);
+    page.waitForSelector(`select[data-drupal-selector="edit-langcode"]`);
+    let lang = true;
+    while(lang) {
+        const answer = await select({
+            message: 'Select language',
+            choices: (await page.evaluate(() => {
+                return Array.from(document.querySelectorAll('select[data-drupal-selector="edit-langcode"] option')).map(item=> ({
+                    name: item.textContent?.trim(),
+                    value: item.value
+                }))
+            })),
+            default: 'en'
+        });
+        await page.select(`select[data-drupal-selector="edit-langcode"]`, answer);
+        await press(page, `input[data-drupal-selector="edit-submit"]`);
+        if(await page.$(`a[href="/core/install.php?langcode=${answer}"]`)){
+            console.error('The installer requires to contact the translation server to download a translation file. Check your internet connection and verify that your website can reach the translation server at https://ftp.drupal.org.');
+            await page.goBack();
+        } else {
+            lang = false;
         }
-        //await press(page, `input[data-drupal-selector="edit-submit"]`);
-    } else {
-        console.log('skipping...');
-        //await press(page, `input[data-drupal-selector="edit-skip"]`);
     }
+
+    await page.waitForSelector(`div.js-form-type-radio`);
+    const answer = await select({
+        message: 'Select an installation profile',
+        choices: (await page.evaluate(() => 
+        Array.from(document.querySelectorAll('.js-form-type-radio'))
+        .map(item => {
+            return {
+                value: item.querySelector('input[type="radio"]')?.getAttribute('value'),
+                name: item.querySelector('label')?.textContent?.trim(),
+                description: item.querySelector('div.form-item__description')?.textContent?.trim() || ''
+            }
+        })))
+    });
+    await page.click(`input[value="${answer}"]`);
+    await press(page, `input[data-drupal-selector="edit-submit"]`);
+
+    if(await page.$(`div[class="claro-details__wrapper"]`)) {
+        console.warn(`PHP OPcode caching can improve your site's performance considerably. It is highly recommended to have OPcache installed on your server. Site OPcache: http://php.net/manual/opcache.installation.php`);
+        alert('Press ^C to stop a prograam, or press Enter to continue');
+        await press(page, `a[href="/core/install.php?langcode=en&profile=standard&continue=1"]`)
+    }
+
+    console.log('next')
 })();
